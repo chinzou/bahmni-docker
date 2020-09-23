02732164123c9b9bad4be41811d80c417cffb08b
chinzou <chine.zoheir@gmail.com>
2020-09-23 13:53:11 +0800

    INFRA-243: Update travis build order5

--- docker_push.sh
+++ docker_push.sh
@@ -10,5 +10,5 @@ do
     docker manifest create $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD) --amend $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_arm64 --amend $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_amd64
     docker manifest create $DOCKER_USERNAME/${service}:latest --amend $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_arm64 --amend $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_amd64
     docker manifest push $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)
-    docker manifest push $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)
+    docker manifest push $DOCKER_USERNAME/${service}:latest
 done
